97b47e0f57cdb0241fdf9c984f35326eb121d452
Alexey Kryazhev <ubivaza@gmail.com>
2017-12-05 22:39:26 +0300

    React: refactor build

--- plugins/react/rollup.config.js
+++ plugins/react/rollup.config.js
@@ -1,27 +1,24 @@
 import babel from 'rollup-plugin-babel';
 import eslint from 'rollup-plugin-eslint';
 
+const globals = {
+  react: 'React',
+  imask: 'IMask',
+  'prop-types': 'PropTypes',
+};
 
 export default {
   input: 'src/index.js',
   name: 'ReactIMask',
-  output: [{
+  output: {
     file: 'dist/react-imask.js',
     format: 'umd'
-  }],
+  },
   sourcemap: true,
   plugins: [
     eslint({configFile: '.eslintrc'}),
     babel(),
   ],
-  external: [
-    'react',
-    'prop-types',
-    'imask',
-  ],
-  globals: {
-    react: 'React',
-    imask: 'IMask',
-    'prop-types': 'PropTypes',
-  }
+  external: Object.keys(globals),
+  globals
 }
