2b04847b45313108532251eeae571c443718b8d0
ivanov-v <sferamail33@yandex.ru>
2019-02-13 23:22:51 +0300

    types added

--- packages/imask/index.d.ts
+++ packages/imask/index.d.ts
@@ -12,10 +12,48 @@ export class MaskElement {
     unbindEvents(): void;
 }
 
-interface Options {
+export type Element = MaskElement | HTMLInputElement | HTMLTextAreaElement;
+
+interface MaskOptions {
     [key: string]: any;
 }
 
-export default class IMask {
-    constructor(el: MaskElement | HTMLInputElement | HTMLTextAreaElement, options: Options)
+export class InputMask {
+    el: MaskElement;
+    masked: any; // ?
+
+    constructor(el: Element, opts: MaskOptions)
+
+    get mask(): any; // : Mask
+    set mask(mask: any); // mask: Mask
+
+    get value(): string;
+    set value(str: string);
+
+    get unmaskedValue(): string;
+    set unmaskedValue(str: string);
+
+    get typedValue(): any;
+    set typedValue(val: any);
+
+    get selectionStart(): number;
+
+    get cursorPos(): number;
+    set cursorPos(pos: number);
+
+    updateValue();
+    updateControl();
+    updateOptions(opts: MaskOptions);
+    updateCursor(cursorPos: number);
+    alignCursor();
+    alignCursorFriendly();
+
+    on(ev: string, handler: Function): this;
+    off(ev: string, handler: Function): undefined | this;
+
+    destroy();
+}
+
+export default class IMask extends InputMask {
+    static InputMask = InputMask;
 }
