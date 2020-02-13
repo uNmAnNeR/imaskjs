fcf078e20b1595cb6f6aa019e96093fa5d84af95
cvaize <cvaize@gmail.com>
2020-02-13 17:24:54 +0500

    (Fix) https://github.com/uNmAnNeR/imaskjs/issues/300

--- packages/imask/src/polyfills.js
+++ packages/imask/src/polyfills.js
@@ -1,5 +1,5 @@
+import 'core-js/features/global-this';
 import 'core-js/features/object/assign';
 import 'core-js/features/string/repeat';
 import 'core-js/features/string/pad-start';
 import 'core-js/features/string/pad-end';
-import 'core-js/features/global-this';
