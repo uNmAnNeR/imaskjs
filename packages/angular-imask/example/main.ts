db5160d638b38ba51acb98d25f14a2d1775c4644
Kirill Shatskiy <shaackiy@gmail.com>
2020-04-25 19:32:42 +0300

    remove import of imask module from the directive

--- packages/angular-imask/example/main.ts
+++ packages/angular-imask/example/main.ts
@@ -3,6 +3,7 @@ import { enableProdMode } from '@angular/core';
 
 import { AppModule } from './app/app.module';
 
+enableProdMode();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
---
+++ packages/angular-imask/src/directive.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { IMaskDirective } from './imask.directive';
+import { IMaskFactory } from './imask-factory';
+import { DefaultImaskFactory } from './default-imask-factory';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [IMaskDirective],
+  providers: [{provide: IMaskFactory, useClass: DefaultImaskFactory}],
+  exports: [IMaskDirective]
+})
+export class IMaskDirectiveModule {}
