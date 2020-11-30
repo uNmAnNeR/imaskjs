11913ce2cd3e145005fbcc2a952de17f22647f31
Nichols, Kieran <kieran.nichols@tylertech.com>
2020-11-30 11:33:50 -0500

    fix(utils): escape forward slash in regular expression for compatibility with strict minifiers

--- packages/imask/src/core/utils.js
+++ packages/imask/src/core/utils.js
@@ -68,7 +68,7 @@ function forceDirection (direction: Direction): Direction {
 /** Escapes regular expression control chars */
 export
 function escapeRegExp (str: string): string {
-  return str.replace(/([.*+?^=!:${}()|[\]/\\])/g, '\\$1');
+  return str.replace(/([.*+?^=!:${}()|[\]\/\\])/g, '\\$1');
 }
 
 
