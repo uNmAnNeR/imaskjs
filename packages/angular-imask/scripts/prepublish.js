b99264ffa8f6f8409a1fea8b8ea68120267f3653
Alexey Kryazhev <ubivaza@gmail.com>
2021-02-11 21:12:40 +0300

    fix angular plugin prepublish script to use newest version of core

--- packages/angular-imask/scripts/prepublish.js
+++ packages/angular-imask/scripts/prepublish.js
@@ -5,4 +5,5 @@ const version = require('../package.json').version;
 const distPackagePath = path.join(__dirname, '..', 'dist', 'package.json');
 const distPackage = JSON.parse(fs.readFileSync(distPackagePath));
 distPackage.version = version;
+distPackage.dependencies.imask = '^' + version;
 fs.writeFileSync(distPackagePath, JSON.stringify(distPackage, null, '  '));
