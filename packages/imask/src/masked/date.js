33a065fa9b1e63c4c89f805d2eecc44f12b4ba95
Алексей Кряжев <ubivaza@gmail.com>
2022-02-02 08:33:43 +0300

    fix #314

--- packages/imask/src/masked/date.js
+++ packages/imask/src/masked/date.js
@@ -101,6 +101,8 @@ class MaskedDate extends MaskedPattern {
 MaskedDate.DEFAULTS = {
   pattern: 'd{.}`m{.}`Y',
   format: date => {
+    if (!date) return '';
+
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const year = date.getFullYear();
