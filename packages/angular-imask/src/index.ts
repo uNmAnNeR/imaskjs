d71ec449e5a711cf9b618b45bf1451f122ff74fb
Алексей Кряжев <ubivaza@gmail.com>
2023-06-19 13:31:28 +0300

    fix #875

--- packages/angular-imask/src/index.ts
+++ packages/angular-imask/src/index.ts
@@ -1,4 +1,4 @@
-import { default as IMask } from 'imask';
+export { default as IMask } from 'imask';
 export * from './imask.directive';
 export * from './imask.pipe';
 export * from './imask.module';
